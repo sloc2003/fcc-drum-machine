251eab063d266c8c5056a39c299a68701c25d292
Oscar E. Zavala <sloc2003@gmail.com>
2019-11-11 12:26:54 -0700

    Not a working Branch trying to fix 'freesound.js' to work with the package may need to abandon it, but it's interesting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+import freesound from "./freesound.js";
 import * as serviceWorker from "./serviceWorker";
 
 class ThisClass extends React.Component {
@@ -12,21 +12,87 @@ class ThisClass extends React.Component {
       alias_1: "nick1",
       alias_2: "nick2"
     };
+    this.playSound = this.playSound.bind(this);
   }
+
+  playSound(event) {
+    console.log(event.target.querySelector("audio"));
+  }
+
+  componentDidMount() {
+    freesound.setToken("RBLFr36UxS9OMw2xTi5EH5TU5dPoFXKdogfifa9J");
+    var fields = "id,name,url";
+    freesound.getPack(17,
+    (pack) => {
+      pack.sounds(
+        (sounds) => {
+          console.log(sounds.id)
+        },
+          null
+      )
+  },
+  () => console.log('errorNoSuccess')
+    )
+}
 
   render() {
     return (
       <div id="drum-machine">
         <div className="drum-pads">
-          <DrumPad id="Q" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="W" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="E" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="A" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="S" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="D" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="Z" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="X" audioID="unique-audio-ID" src="audio-src"/>
-          <DrumPad id="C" audioID="unique-audio-ID" src="audio-src"/>
+          <DrumPad
+            id="Q"
+            audioID="unique-audio-ID"
+            src="some-source"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="W"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="E"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="A"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="S"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="D"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="Z"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="X"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
+          <DrumPad
+            id="C"
+            audioID="unique-audio-ID"
+            src="audio-src"
+            clickHandler={this.playSound}
+          />
         </div>
         <div id="display">My Display</div>
       </div>
@@ -34,14 +100,19 @@ class ThisClass extends React.Component {
   }
 }
 
-const DrumPad = (props) => {
+const DrumPad = props => {
   return (
-    <button className="drum-pad" id={props.audioID} type="button">
+    <button
+      className="drum-pad"
+      id={props.audioID}
+      type="button"
+      onClick={props.clickHandler}
+    >
       {props.id}
       <audio src={props.src} className="clip" id={props.id} />
     </button>
   );
-}
+};
 
 ReactDOM.render(<ThisClass />, document.getElementById("root"));
 
